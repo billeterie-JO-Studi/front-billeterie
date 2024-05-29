006f3c9e171025c67047646aed55dcf2d318b084
Rémi PONCHE <ponche62880@gmail.com>
2024-05-29 02:27:18 +0200

    👔 Modification hook useAuthentification
    modification pour stocker le jeton JWT dans le localStorage afin de persité la connexion avec des actualisation du navigateur

--- src/hooks/useAuthentification.tsx
+++ src/hooks/useAuthentification.tsx
@@ -1,8 +1,6 @@
-import axios from "axios"
+import axios from "axios";
 import { useSetRecoilState } from "recoil";
 import { userState } from "../store/store";
-
-
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -11,23 +9,24 @@ type LoginUser = {
   password: string;
 };
 
-
 export default function useAuthentification() {
-
   const setUser = useSetRecoilState(userState);
 
   const login = async (loginUser: LoginUser) => {
+    
     const response = await axios.post(`${apiUrl}/api/auth/local`, {
       identifier: loginUser.identifier,
       password: loginUser.password,
     });
 
+    localStorage.setItem('jwt', response.data.jwt);   
     setUser({ token: response.data.jwt, isConnected: true });
-  }
+  };
 
   const logout = async () => {
-    setUser({ token: "", isConnected: false}); 
-  }
+    setUser({ token: "", isConnected: false });
+    localStorage.removeItem('jwt');
+  };
 
-  return {login, logout}; 
+  return { login, logout };
 }
